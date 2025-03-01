6b06b236830f293ec75f8c1f60da4330cd28333f
ConnorBroughton <connor.broughton@mail.utoronto.ca>
2025-03-01 15:04:42 -0500

    Removed crimes.geojson

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiY29ubm9yYnJvdWdodG9uIiwiYSI6ImNtNmllajk3dDA4MnYya29vcmRhZ3pmMmkifQ.rVteR2UEyh7d5e4JIP3zCA'; 
+mapboxgl.accessToken = 'pk.eyJ1IjoiY29ubm9yYnJvdWdodG9uIiwiYSI6ImNtNmllajk3dDA4MnYya29vcmRhZ3pmMmkifQ.rVteR2UEyh7d5e4JIP3zCA';
 
 const map = new mapboxgl.Map({
-    container: 'my-map', 
-    style: 'mapbox://styles/mapbox/standard', //style URL
-    // alternatively you can use mapbox-owned style from https://docs.mapbox.com/api/maps/styles/#mapbox-styles
-    center: [-79.39, 43.66], 
-    zoom: 12, 
+    container: 'my-map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-79.39, 43.66],
+    zoom: 12,
 });
 
-map.AddSource('crimes', {
-    type: 'geojson',
-    data: 'https://'
-})
+map.on('load', () => {
+    map.addSource('crimes-data', {
+        type: 'vector',
+        url: 'mapbox://connorbroughton.7vwajcfr'
+    });
+
+  
+    map.addLayer({
+        id: 'crimes-data-poly',
+        type: 'fill',
+        source: 'crimes-data',
+        'source-layer': 'crimes-5zigho', 
+        paint: {
+            'fill-color': 'blue',
+            'fill-opacity': 0.5 
+        }
+    });
+});
